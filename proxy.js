bf0852c92889462666283c3699255bf271ee7068
Yurov Dmitry <dmitry.yurov@corp.mail.ru>
2021-12-07 13:05:59 +0300

    fix proxy

--- proxy.js
+++ proxy.js
@@ -4,19 +4,29 @@ const bodyParser = require('body-parser');
 const multer = require('multer')();
 const FormData = require('form-data');
 const request = require('request');
-const axios = require('axios');
+const fs = require('fs');
+// const axios = require('axios');
 
 app.use(bodyParser.json());
 
 app.post('/photo', multer.array('image[]'), (req, res) => {
+     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+ 
+     if (req.method === 'OPTIONS') {
+         // CORS Preflight
+         res.send();
+         return;
+     }
+
     const fileRecievedFromClient = req.files[0]; //File Object sent in 'fileFieldName' field in multipart/form-data
     const form = new FormData();
     form.append('image[]', fileRecievedFromClient.buffer, fileRecievedFromClient.originalname);
     const targetURL = 'https://9may.mail.ru'; // Target-URL ie. https://example.com or http://example.com
     const headers = {
-        ...req.headers,
         origin: 'https://9may.mail.ru',
-        host: '9may.mail.ru',
         ...form.getHeaders(),
     };
     const options = {
